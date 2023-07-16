25ed1cc3e3a58c81b4d7a9b6c9130223e09d1cef
ZerfyT <nipdili1143@gmail.com>
2023-07-16 13:32:07 +0530

    todo API

---
+++ app/Http/Controllers/API/TodoController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return response()->json($todos, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $todo = Todo::create($request->all());
+        $todo->save();
+
+        return response()->json($todo, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        return response()->json($todo, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->update($request->all());
+        return response()->json($todo, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+        return response()->json(null, 204);
+    }
+}
